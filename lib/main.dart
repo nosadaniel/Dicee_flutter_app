d113ccab680d4b7008d0bcc660e2e6263905c258
nosadaniel <nosadaniel02@gmail.com>
2020-01-19 18:20:15 +0100

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.teal,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text('Dicee by Nosa'),
+          backgroundColor: Colors.grey,
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,42 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int rightDice = 1;
+  int leftDice = 6;
+  void changeDices() {
+    setState(() {
+      rightDice = Random().nextInt(6) + 1;
+      leftDice = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  changeDices();
+                },
+                child: Image.asset('images/dice$leftDice.png')),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  changeDices();
+                },
+                child: Image.asset('images/dice$rightDice.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
